745fa180649f40f8aea2ffde3cb1ca671a76e2ed
OkaOka_520 <85399297+OkaOka-502@users.noreply.github.com>
2021-06-24 18:26:40 +0700

    Menambahkan beberapa input

--- input_bio.py
+++ input_bio.py
@@ -1,6 +1,9 @@
 class Biodata :
-  def __init__ (self, name, grade, age, hobby, address, n_phone,email) :
-    self.n = (name)
+  def __init__ (self,full_name, nickname, gender, birth, grade, age, hobby, address, n_phone, email) :
+    self.f = (full_name)
+    self.n = (nickname)
+    self.r = (gender)
+    self.b = (birth)
     self.g = (grade)
     self.a = (age)
     self.h = (hobby)
@@ -9,10 +12,13 @@ class Biodata :
     self.e = (email)
     
   def bio (self) :
-    print (self.n, self.g, self.a, self.h, self.d, self.p, self.e)
+    print (self.f, self.n, self.r, self.b, self.g, self.a, self.h, self.d, self.p, self.e)
+
     
-    
-name = input('Name : ')
+full_name = input('Full Name : ')
+nickname = input('Nickname : ')
+gender = input('Gender : ')
+birth = input('Place Date of Birth: ')
 grade = input('Grade : ')
 age = input('Age : ')
 hobby = input('Hobby : ')
@@ -20,5 +26,5 @@ address = input('Address : ')
 n_phone = input('Number Phone : ')
 email = input('E-mail : ')
 
-bi = Biodata(name, grade, age, hobby, address, n_phone,email)
+bi = Biodata (full_name, nickname, gender, birth, grade, age, hobby, address, n_phone, email)
 print(bi.bio())
